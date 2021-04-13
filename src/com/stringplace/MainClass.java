3febc01b8cdba5c174dc5766c56afa22c0754a54
kunpengzhanchi138 <50365191+kunpengzhanchi138@users.noreply.github.com>
2021-04-13 22:48:48 +0800

    Update MainClass.java

--- src/com/stringplace/MainClass.java
+++ src/com/stringplace/MainClass.java
@@ -3,8 +3,6 @@ package com.stringplace;
 public class MainClass {
     public static void main(String[] args) {
         System.out.println("zhuanbei123");
-        System.out.println("zhuanbei123");
-        System.out.println("zhuanbei123");
         
     }
 }
