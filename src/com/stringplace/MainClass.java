abe2c1b9fdcf4f9d49fb258ff039496513009a9a
kunpengzhanchi138 <kunpengzhanchi138@163.com>
2021-04-13 22:48:08 +0800

    添加一行输出

--- src/com/stringplace/MainClass.java
+++ src/com/stringplace/MainClass.java
@@ -4,6 +4,7 @@ public class MainClass {
     public static void main(String[] args) {
         System.out.println("zhuanbei123");
         System.out.println("zhuanbei123");
+        System.out.println("zhuanbei123");
         
     }
 }
