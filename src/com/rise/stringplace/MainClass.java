b113571941ab117a42460d8f843040e819fb4830
kunpengzhanchi138 <kunpengzhanchi138@163.com>
2021-08-30 16:48:56 +0800

    代码合并

---
+++ src/com/rise/stringplace/MainClass.java
@@ -0,0 +1,13 @@
+package com.rise.stringplace;
+
+public class MainClass {
+    public static void main(String[] args) {
+       String str=dayin();
+        System.out.println(str);
+        
+    }
+
+    public static String dayin(){
+        return "fanhuizhi";
+    }
+}
